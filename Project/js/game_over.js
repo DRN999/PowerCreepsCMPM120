00ee9edc2a201d60ceafa96990731e2f9a6a7083
Noriaki Nakano <nnakano@ucsc.edu>
2017-05-10 16:02:42 -0700

    init

---
+++ Project/js/game_over.js
@@ -0,0 +1,21 @@
+// play state of the game 
+
+var game_over = 
+{// the game_over screen 
+	
+	preload: function()
+	{// preload assets
+		
+		
+	},// End preload 
+	
+	create: function()
+	{// create play_state
+		
+		console.log('gameover:create');
+		
+	}// End create 
+
+	
+	
+};
---
+++ Project/js/init.js
@@ -0,0 +1,21 @@
+// Noriaki Nakano 
+// nnakano@ucsc.edu 
+// init variables 
+var game;
+var player;
+var enemies = new Array();
+
+var move_up = false;
+var move_right = false;
+var move_left = false;
+var move_down = false;
+
+
+var images = // images that needs to be imported 
+[
+	'assets/img/Square.jpg',
+	'assets/img/search.png'
+];
+
+
+
