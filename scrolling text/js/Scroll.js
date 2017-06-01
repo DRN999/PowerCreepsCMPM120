88ced4844b76afdaa92cfd1a72b90aa3407f2524
Nathaniel <nsuriawi@ucsc.edu>
2017-06-01 15:29:59 -0700

    basic scrolling text

--- scrolling text/js/Scroll.js
+++ scrolling text/js/Scroll.js
@@ -1,41 +1,72 @@
-var canvas_width = 1280;
-var canvas_height = 720;
-var fontHeight= 16;
-var game = new Phaser.Game(canvas_width, canvas_height, Phaser.AUTO, 'phaser', { preload: preload, create: create});
+var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'phaser-example', { create: create });
 
+var content = [
+    "I overheard the barkeep's conversation with the captain of nightwatch. ",
+	"He said something along the lines of . . .",
+	". . .",
+    "Barkeep: It's going to be a big night, ain't it?",
+    "Captain: I sure hope so, 20 years and this is the thanks I get,",
+	". . .",
+    "It sounds like the guards will be throwing the chief a party, that probably means a less guards on duty tonight.",
+    "Should I try to make my way through during the day or at night?",
+    "Visibility might be low though . . .",
 
-function preload() {
+];
 
-    game.load.bitmapFont('Arial', 'assets/fonts/sample.xml.tga');
+var line = [];
 
-}
+var wordIndex = 0;
+var lineIndex = 0;
 
+var wordDelay = 75;
+var lineDelay = 300;
 
 function create() {
-    var message = "Glen's Invaders";
 
-    var textObject = game.add.bitmapText(game.world.centerX, 10, 'Arial', message, fontHeight);
-    textObject.x = game.width / 2  - textObject.textWidth / 2;
+    text = game.add.text(32, 32, '', { font: "18px Arial", fill: "#ffffff" });
 
-    displayLetterByLetterText(textObject, message, function() {
-        // stuff you want to do at the end of the animation
-        // eg. this.input.onDown.addOnce(this.start, this);
-    });
-}
-
-
-function displayNextLetter() {
-
-    this.textObject.text = this.message.substr(0, this.counter);
-    this.counter += 1;
+    nextLine();
 
 }
 
-function displayLetterByLetterText(textObject, message, onCompleteCallback) {
+function nextLine() {
 
-    var timerEvent = game.time.events.repeat(80, message.length, displayNextLetter, 
-                                { textObject: textObject, message: message, counter: 1 });
+    if (lineIndex === content.length)
+    {
+        //  We're finished
+        return;
+    }
 
-    timerEvent.timer.onComplete.addOnce(onCompleteCallback, this);
+    //  Split the current line on spaces, so one word per array element
+    line = content[lineIndex].split(' ');
+
+    //  Reset the word index to zero (the first word in the line)
+    wordIndex = 0;
+
+    //  Call the 'nextWord' function once for each word in the line (line.length)
+    game.time.events.repeat(wordDelay, line.length, nextWord, this);
+
+    //  Advance to the next line
+    lineIndex++;
+
+}
+
+function nextWord() {
+
+    //  Add the next word onto the text string, followed by a space
+    text.text = text.text.concat(line[wordIndex] + " ");
+
+    //  Advance the word index to the next word in the line
+    wordIndex++;
+
+    //  Last word?
+    if (wordIndex === line.length)
+    {
+        //  Add a carriage return
+        text.text = text.text.concat("\n");
+
+        //  Get the next line after the lineDelay amount of ms has elapsed
+        game.time.events.add(lineDelay, nextLine, this);
+    }
 
 }
